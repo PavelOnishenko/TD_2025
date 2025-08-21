23eec6720aae88168c8f47ce9599c9216bd7261f
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2025-08-22 00:36:07 +0200

    feat: add tower color property

--- src/Tower.js
+++ src/Tower.js
@@ -1,11 +1,12 @@
 export default class Tower {
-    constructor(x, y) {
+    constructor(x, y, color = 'red') {
         this.x = x;
         this.y = y;
         this.w = 40;
         this.h = 40;
         this.range = 120;
         this.lastShot = 0;
+        this.color = color;
     }
 
     center() {
@@ -19,10 +20,12 @@ export default class Tower {
         const c = this.center();
         ctx.beginPath();
         ctx.arc(c.x, c.y, this.range, 0, Math.PI * 2);
-        ctx.strokeStyle = 'rgba(0,0,255,0.3)';
+        ctx.strokeStyle = this.color === 'red'
+            ? 'rgba(255,0,0,0.3)'
+            : 'rgba(0,0,255,0.3)';
         ctx.stroke();
 
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
 }
