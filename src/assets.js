40e32a761523d3294a354f12dbace69146f08b2a
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2025-09-20 20:17:38 +0200

    Tower level 3 sprite is on.

--- src/assets.js
+++ src/assets.js
@@ -6,9 +6,11 @@ export async function loadAssets() {
     const tower_1b = await loadImage('assets/tower_1B.png');
     const tower_2r = await loadImage('assets/tower_2R.png');
     const tower_2b = await loadImage('assets/tower_2B.png');
+    const tower_3r = await loadImage('assets/tower_3R.png');
+    const tower_3b = await loadImage('assets/tower_3B.png');
     const swarm_r = await loadImage('assets/swarm_R.png');
     const swarm_b = await loadImage('assets/swarm_B.png');
-    return {bg, cell, tower_1r, tower_1b, tower_2r, tower_2b, swarm_r, swarm_b};
+    return {bg, cell, tower_1r, tower_1b, tower_2r, tower_2b, tower_3r, tower_3b, swarm_r, swarm_b};
 }
 
 function loadImage(url) {
