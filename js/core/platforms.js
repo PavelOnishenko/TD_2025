4c6afa06bac3b8e958f72a2d1f1c35cd697d252f
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2025-10-05 22:46:21 +0200

    fix.

--- js/core/platforms.js
+++ js/core/platforms.js
@@ -2,7 +2,7 @@ import Platform from '../entities/Platform.js';
 
 const PLATFORM_CONFIGS = [
     { xFactor: 0.5, yFactor: 0.35, scale: 0.5 },
-    { xFactor: 0.5, yFactor: 0.58, scale: 1.5 }
+    { xFactor: 0.5, yFactor: 0.58, scale: 0.5 }
 ];
 
 export function createPlatforms({ width, height }) {
