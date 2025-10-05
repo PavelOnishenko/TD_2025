2eb4294609d7adade69c09062bf906ba1a5b5aaf
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2025-10-05 22:46:04 +0200

    Platform size fix.

--- js/core/platforms.js
+++ js/core/platforms.js
@@ -1,8 +1,8 @@
 import Platform from '../entities/Platform.js';
 
 const PLATFORM_CONFIGS = [
-    { xFactor: 0.5, yFactor: 0.35, scale: 0.85 },
-    { xFactor: 0.5, yFactor: 0.58, scale: 1.05 }
+    { xFactor: 0.5, yFactor: 0.35, scale: 0.5 },
+    { xFactor: 0.5, yFactor: 0.58, scale: 1.5 }
 ];
 
 export function createPlatforms({ width, height }) {
