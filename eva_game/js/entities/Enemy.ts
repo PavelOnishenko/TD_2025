d56dfe8c5b878243da322b5bcdbb8652998c0b0a
Pavel Onishchenko <pavelonishchenko.software@gmail.com>
2026-01-25 17:09:33 +0100

    config.

--- eva_game/js/entities/Enemy.ts
+++ eva_game/js/entities/Enemy.ts
@@ -174,7 +174,7 @@ export default class Enemy extends Entity {
         const positionThreshold = balanceConfig.attackPosition.positionReachedThreshold;
 
         // Check if we're close enough to the target position to stop
-        const reachedTarget: boolean = distance <= positionThreshold;
+        const reachedTarget: boolean = distance + balanceConfig.attackPosition.attackPointThreshold <= positionThreshold;
 
         this.updateFacingDirection(dx);
 
