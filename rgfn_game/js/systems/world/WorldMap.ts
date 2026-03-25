dd099237e82eff9b979fe1880bd71a0d648d07ea
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2026-03-25 20:06:36 +0100

    RGFN: enable Selected panel hover info on battle map

--- rgfn_game/js/systems/world/WorldMap.ts
+++ rgfn_game/js/systems/world/WorldMap.ts
@@ -1118,6 +1118,7 @@ export default class WorldMap {
             && this.selectedGridPos.row === this.playerGridPos.row;
 
         return {
+            mode: 'world',
             col: this.selectedGridPos.col,
             row: this.selectedGridPos.row,
             terrainType: terrain.type,
