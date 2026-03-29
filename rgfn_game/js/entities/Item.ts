a52a7fcaf56e1d84a8bd256a5c038ae5e964816e
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2026-03-29 20:31:03 +0200

    Manual Item refac.

--- rgfn_game/js/entities/Item.ts
+++ rgfn_game/js/entities/Item.ts
@@ -61,63 +61,23 @@ const WEAPON_VARIANTS: Array<{
     attackRange?: number;
 
 }> = [
-    {
-        key: 'knife',
-        baseName: 'Knife',
-        damageBonuses: [1, 2, 3, 4],
-        handsRequired: 1,
-        requirements: { agility: 2 },
-        goldValue: 2,
-        findWeight: 10,
-    },
-    {
-        key: 'shortSword',
-        baseName: 'Short Sword',
-        damageBonuses: [2, 3, 4, 5],
-        handsRequired: 1,
-        requirements: { agility: 4, strength: 2 },
-        goldValue: 5,
+    { key: 'knife', baseName: 'Knife', damageBonuses: [1, 2, 3, 4], handsRequired: 1, requirements: { agility: 2 }, goldValue: 2, findWeight: 10, },
+    { 
+        key: 'shortSword', baseName: 'Short Sword', damageBonuses: [2, 3, 4, 5], handsRequired: 1, requirements: { agility: 4, strength: 2 }, goldValue: 5, 
         findWeight: 9,
     },
+    { key: 'axe', baseName: 'Axe', damageBonuses: [3, 4, 5, 6], handsRequired: 1, requirements: { agility: 4, strength: 6 }, goldValue: 7, findWeight: 7, },
     {
-        key: 'axe',
-        baseName: 'Axe',
-        damageBonuses: [3, 4, 5, 6],
-        handsRequired: 1,
-        requirements: { agility: 4, strength: 6 },
-        goldValue: 7,
-        findWeight: 7,
+        key: 'twoHandedSword', baseName: 'Two-Handed Sword', damageBonuses: [9, 11, 13, 14], handsRequired: 2, requirements: { agility: 4, strength: 12 },
+        goldValue: 18, findWeight: 5,
     },
     {
-        key: 'twoHandedSword',
-        baseName: 'Two-Handed Sword',
-        damageBonuses: [9, 11, 13, 14],
-        handsRequired: 2,
-        requirements: { agility: 4, strength: 12 },
-        goldValue: 18,
-        findWeight: 5,
+        key: 'bow', baseName: 'Bow', damageBonuses: [1, 2, 3, 4], handsRequired: 2, requirements: { agility: 8, strength: 2 }, goldValue: 12, findWeight: 3, 
+        isRanged: true, attackRange: 2,
     },
     {
-        key: 'bow',
-        baseName: 'Bow',
-        damageBonuses: [1, 2, 3, 4],
-        handsRequired: 2,
-        requirements: { agility: 8, strength: 2 },
-        goldValue: 12,
-        findWeight: 3,
-        isRanged: true,
-        attackRange: 2,
-    },
-    {
-        key: 'crossbow',
-        baseName: 'Crossbow',
-        damageBonuses: [3, 4, 5, 6],
-        handsRequired: 2,
-        requirements: { agility: 10, strength: 6 },
-        goldValue: 20,
-        findWeight: 2,
-        isRanged: true,
-        attackRange: 3,
+        key: 'crossbow', baseName: 'Crossbow', damageBonuses: [3, 4, 5, 6], handsRequired: 2, requirements: { agility: 10, strength: 6 }, goldValue: 20, 
+        findWeight: 2, isRanged: true, attackRange: 3,
     },
 ];
 
@@ -130,41 +90,18 @@ const ARMOR_VARIANTS: Array<{
     goldValue: number;
     spriteClass: string;
 }> = [
+    { id: 'armor_t1', name: 'Armor +1', description: '+1 armor', effects: { flatArmor: 1 }, findWeight: 1, goldValue: 12, spriteClass: 'armor-t1-sprite' },
     {
-        id: 'armor_t1',
-        name: 'Armor +1',
-        description: '+1 armor',
-        effects: { flatArmor: 1 },
-        findWeight: 1,
-        goldValue: 12,
-        spriteClass: 'armor-t1-sprite'
+        id: 'armor_t2', name: 'Armor +1 Guarded', description: '+1 armor and 20% damage decrease', effects: { flatArmor: 1, damageReductionPercent: 0.2 }, 
+        findWeight: 1, goldValue: 16, spriteClass: 'armor-t2-sprite'
     },
     {
-        id: 'armor_t2',
-        name: 'Armor +1 Guarded',
-        description: '+1 armor and 20% damage decrease',
-        effects: { flatArmor: 1, damageReductionPercent: 0.2 },
-        findWeight: 1,
-        goldValue: 16,
-        spriteClass: 'armor-t2-sprite'
+        id: 'armor_t3', name: 'Armor +2 Fragile', description: '+2 armor, but can only absorb 20 HP', effects: { flatArmor: 2, maxAbsorbHp: 20 }, findWeight: 1, 
+        goldValue: 18, spriteClass: 'armor-t3-sprite'
     },
     {
-        id: 'armor_t3',
-        name: 'Armor +2 Fragile',
-        description: '+2 armor, but can only absorb 20 HP',
-        effects: { flatArmor: 2, maxAbsorbHp: 20 },
-        findWeight: 1,
-        goldValue: 18,
-        spriteClass: 'armor-t3-sprite'
-    },
-    {
-        id: 'armor_t4',
-        name: 'Armor Bulwark',
-        description: '50% damage decrease, can only absorb 20 HP',
-        effects: { damageReductionPercent: 0.5, maxAbsorbHp: 20 },
-        findWeight: 1,
-        goldValue: 20,
-        spriteClass: 'armor-t4-sprite'
+        id: 'armor_t4', name: 'Armor Bulwark', description: '50% damage decrease, can only absorb 20 HP', effects: { damageReductionPercent: 0.5, maxAbsorbHp: 20 },
+        findWeight: 1, goldValue: 20, spriteClass: 'armor-t4-sprite'
     },
 ];
 
@@ -188,26 +125,16 @@ const generatedWeapons: ItemData[] = WEAPON_VARIANTS.flatMap((variant) => (
     })
 ));
 
-export const BOW_ITEM: ItemData = {
-    ...generatedWeapons.find((item) => item.id === 'bow_t1')!,
-    id: 'bow',
-    name: 'Bow',
-};
+ // todo is this needed? Dead code check. We destroy dead code. If it's needed, can it be processed normally instead like all other weapons? Do it.
+export const BOW_ITEM: ItemData = {...generatedWeapons.find((item) => item.id === 'bow_t1')!, id: 'bow', name: 'Bow', };
 
 export const HEALING_POTION_ITEM: ItemData = {
-    id: 'healingPotion',
-    name: 'Healing Potion',
-    description: 'A restorative potion that heals 5 HP when used',
-    type: 'consumable',
-    goldValue: 4,
+    id: 'healingPotion', name: 'Healing Potion', description: 'A restorative potion that heals 5 HP when used', type: 'consumable', goldValue: 4, 
     spriteClass: 'potion-sprite',
 };
 
 export const MANA_POTION_ITEM: ItemData = {
-    id: 'manaPotion',
-    name: 'Mana Potion',
-    description: 'A restorative potion that recovers mana when used',
-    type: 'consumable'
+    id: 'manaPotion', name: 'Mana Potion', description: 'A restorative potion that recovers mana when used', type: 'consumable'
 };
 
 export const ITEM_LIBRARY: ItemData[] = [
@@ -216,14 +143,8 @@ export const ITEM_LIBRARY: ItemData[] = [
     BOW_ITEM,
     ...generatedWeapons.filter((item) => item.id !== 'bow_t1'),
     ...ARMOR_VARIANTS.map((armor) => ({
-        id: armor.id,
-        name: armor.name,
-        description: armor.description,
-        type: 'armor' as const,
-        effects: armor.effects,
-        goldValue: armor.goldValue,
-        findWeight: armor.findWeight,
-        spriteClass: armor.spriteClass,
+        id: armor.id, name: armor.name, description: armor.description, type: 'armor' as const, effects: armor.effects, goldValue: armor.goldValue,
+        findWeight: armor.findWeight, spriteClass: armor.spriteClass,
     })),
 ];
 
