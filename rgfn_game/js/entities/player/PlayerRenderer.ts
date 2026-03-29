96858addb19c9942d832180cd7cd3f35c352cfa9
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2026-03-29 22:26:56 +0200

    PlayerReneder todos.

--- rgfn_game/js/entities/player/PlayerRenderer.ts
+++ rgfn_game/js/entities/player/PlayerRenderer.ts
@@ -30,6 +30,7 @@ export default class PlayerRenderer {
         ctx.fill();
     }
 
+    // todo rule 17 should be applied to this parameter list (probably one liner would do here). Make sure linter triggers on such situations. Edit linter if needed. 
     private drawBody(
         ctx: CanvasRenderingContext2D,
         screenX: number,
@@ -75,6 +76,7 @@ export default class PlayerRenderer {
         ctx.fillRect(left + width - 7, top + 12, 5, 4);
     }
 
+    // todo rule 17 should be applied to this parameter list (probably one liner would do here).
     private drawHealthBar(
         ctx: CanvasRenderingContext2D,
         player: PlayerRenderState,
