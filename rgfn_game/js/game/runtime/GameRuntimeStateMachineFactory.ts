864d2384439bfa432ae730a0a17014f996a430b2
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2026-03-30 19:47:35 +0200

    Refactor GameFactory runtime wiring into dedicated classes

---
+++ rgfn_game/js/game/runtime/GameRuntimeStateMachineFactory.ts
@@ -0,0 +1,20 @@
+import StateMachine from '../../utils/StateMachine.js';
+import WorldMap from '../../systems/world/WorldMap.js';
+import Skeleton from '../../entities/Skeleton.js';
+import { TerrainType } from '../../types/game.js';
+import GameModeStateMachine from '../../systems/game/runtime/GameModeStateMachine.js';
+import GameVillageCoordinator from '../../systems/game/runtime/GameVillageCoordinator.js';
+import { GameFacade, UIBundle } from '../GameFacade.js';
+
+export default class GameRuntimeStateMachineFactory {
+    public static readonly create = (game: GameFacade, ui: UIBundle, worldMap: WorldMap, villageCoordinator: GameVillageCoordinator): StateMachine =>
+        new GameModeStateMachine<{ enemies: Skeleton[]; terrainType: TerrainType }>({
+            onEnterWorld: () => game.worldModeController.enterWorldMode(ui.hudElements.modeIndicator, ui.worldUI.sidebar, ui.battleUI.sidebar, ui.villageUI.sidebar),
+            onUpdateWorld: () => game.worldModeController.updateWorldMode(),
+            onEnterBattle: (battleData) => game.battleCoordinator.enterBattleMode(battleData.enemies, battleData.terrainType),
+            onUpdateBattle: () => game.battleCoordinator.updateBattleMode(),
+            onExitBattle: () => game.battleCoordinator.exitBattleMode(),
+            onEnterVillage: () => game.onVillageEntered(worldMap, villageCoordinator),
+            onExitVillage: () => villageCoordinator.exitVillageMode(),
+        }).create();
+}
---
+++ rgfn_game/js/game/runtime/GameRuntimeUiBinder.ts
@@ -0,0 +1,69 @@
+import { GameFacade } from '../GameFacade.js';
+import GameBattleCoordinator from '../../systems/game/runtime/GameBattleCoordinator.js';
+import GameHudCoordinator from '../../systems/game/runtime/GameHudCoordinator.js';
+import GameUiEventBinder from '../../systems/game/GameUiEventBinder.js';
+import GameVillageCoordinator from '../../systems/game/runtime/GameVillageCoordinator.js';
+import WorldModeController from '../../systems/WorldModeController.js';
+import VillageActionsController from '../../systems/village/VillageActionsController.js';
+import DeveloperEventController from '../../systems/encounter/DeveloperEventController.js';
+import { CombatMove } from '../../systems/combat/DirectionalCombat.js';
+import { UIBundle } from '../GameFacade.js';
+
+export default class GameRuntimeUiBinder {
+    public constructor(
+        private readonly game: GameFacade,
+        private readonly canvas: HTMLCanvasElement,
+        private readonly ui: UIBundle,
+        private readonly villageActionsController: VillageActionsController,
+        private readonly devController: DeveloperEventController,
+        private readonly villageCoordinator: GameVillageCoordinator,
+        private readonly battleCoordinator: GameBattleCoordinator,
+        private readonly worldModeController: WorldModeController,
+        private readonly hudCoordinator: GameHudCoordinator,
+    ) {}
+
+    public bind(): void {
+        new GameUiEventBinder(
+            this.canvas,
+            this.ui.hudElements,
+            this.ui.worldUI,
+            this.ui.battleUI,
+            this.ui.villageUI,
+            this.ui.developerUI,
+            this.villageActionsController,
+            this.devController,
+            this.createHandlers(),
+        ).bind(() => this.villageCoordinator.getVillageName());
+    }
+
+    private readonly createHandlers = () => ({
+        onAttack: () => this.battleCoordinator.handleAttack(),
+        onDirectionalCombatMove: (move: CombatMove) => this.battleCoordinator.handleDirectionalCombatMove(move),
+        onFlee: () => this.battleCoordinator.handleFlee(),
+        onWait: () => this.battleCoordinator.handleWait(),
+        onUsePotionFromBattle: () => this.battleCoordinator.handleUsePotion(true),
+        onUseManaPotionFromBattle: () => this.battleCoordinator.handleUseManaPotion(true),
+        onUsePotionFromHud: () => this.battleCoordinator.handleUsePotion(false),
+        onUseManaPotionFromHud: () => this.battleCoordinator.handleUseManaPotion(false),
+        onUsePotionFromWorld: () => this.battleCoordinator.handleUsePotion(false),
+        onEnterVillageFromWorld: () => this.game.tryEnterVillageFromWorldMap(),
+        onConfirmVillageEntryPrompt: () => this.game.confirmWorldVillageEntry(),
+        onDismissVillageEntryPrompt: () => this.worldModeController.dismissVillageEntryPrompt(),
+        onCampSleepFromWorld: () => this.worldModeController.handleCampSleep(),
+        onNewCharacter: () => this.game.startNewCharacter(),
+        onAddStat: (stat) => this.hudCoordinator.handleAddStat(stat),
+        onRemoveStat: (stat) => this.hudCoordinator.handleRemoveStat(stat),
+        onSaveSkillChanges: () => this.hudCoordinator.handleSaveSkillChanges(),
+        onGodSkillsBoost: () => this.game.onGodSkillsBoost(),
+        onCastSpell: (id) => this.battleCoordinator.handleCastSpell(id),
+        onUpgradeSpell: (id) => this.hudCoordinator.handleUpgradeSpell(id),
+        onCanvasClick: (event) => this.battleCoordinator.handleCanvasClick(event, this.canvas),
+        onCanvasMove: (event) => this.game.handleCanvasMove(event),
+        onCanvasLeave: () => this.game.handleCanvasLeave(),
+        onWorldMapWheel: (event) => this.game.handleWorldMapWheel(event),
+        onWorldMapMiddleDragStart: (event) => this.game.handleWorldMapMiddleDragStart(event),
+        onWorldMapKeyboardZoom: (direction) => this.game.handleWorldMapKeyboardZoom(direction),
+        onCenterWorldMapOnPlayer: () => this.game.centerWorldMapOnPlayer(),
+        onTogglePanel: (panel) => this.hudCoordinator.togglePanel(panel),
+    });
+}
